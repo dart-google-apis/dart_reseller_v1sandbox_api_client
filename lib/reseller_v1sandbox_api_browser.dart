860c84892857023f0613d296253f3f230e57b1d7
Adam Singer <financeCoding@gmail.com>
2014-03-22 19:05:49 -0700

    Automated update

--- lib/reseller_v1sandbox_api_browser.dart
+++ lib/reseller_v1sandbox_api_browser.dart
@@ -8,6 +8,12 @@ import "package:google_reseller_v1sandbox_api/reseller_v1sandbox_api_client.dart
 /** Lets you create and manage your customers and their subscriptions. */
 class Reseller extends Client with BrowserClient {
 
+  /** OAuth Scope2: Manage users on your domain */
+  static const String APPS_ORDER_SCOPE = "https://www.googleapis.com/auth/apps.order";
+
+  /** OAuth Scope2: Manage users on your domain */
+  static const String APPS_ORDER_READONLY_SCOPE = "https://www.googleapis.com/auth/apps.order.readonly";
+
   final oauth.OAuth2 auth;
 
   Reseller([oauth.OAuth2 this.auth]);
--- lib/reseller_v1sandbox_api_console.dart
+++ lib/reseller_v1sandbox_api_console.dart
@@ -9,6 +9,12 @@ import "package:google_reseller_v1sandbox_api/reseller_v1sandbox_api_client.dart
 /** Lets you create and manage your customers and their subscriptions. */
 class Reseller extends Client with ConsoleClient {
 
+  /** OAuth Scope2: Manage users on your domain */
+  static const String APPS_ORDER_SCOPE = "https://www.googleapis.com/auth/apps.order";
+
+  /** OAuth Scope2: Manage users on your domain */
+  static const String APPS_ORDER_READONLY_SCOPE = "https://www.googleapis.com/auth/apps.order.readonly";
+
   final oauth2.OAuth2Console auth;
 
   Reseller([oauth2.OAuth2Console this.auth]);
